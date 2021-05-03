ac0f123ac761fdb6897fd213694dbca84701b8f8
GalinaKr <kravchukgi@gmail.com>
2021-05-03 18:21:31 +0300

    try new one

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,29 +20,5 @@ document.addEventListener("turbolinks:load", () => {
     $('[data-toggle="popover"]').popover()
 })
 
-require("flatpickr")
-import flatpickr from "flatpickr";
-document.addEventListener("turbolinks:load", () => {
-    $('[data-tooltip-display="true"]').tooltip(),
-        flatpickr("[class='flatpickr']", {
-            altInput: true,
-            altFormat: "l, M j",
-            dateFormat: "Y-m-d",
-        })
-})
-
-import * as bootstrapValidate from 'bootstrap-validate';
-window.bootstrapValidate = bootstrapValidate;
-
-import Swal from 'sweetalert2/dist/sweetalert2.js'
-import 'sweetalert2/src/sweetalert2.scss'
-window.Swal = Swal;
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
 import 'images/icons8-search-659.png';
 import 'images/bg-user-img.png';
