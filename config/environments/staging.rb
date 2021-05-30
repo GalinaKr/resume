4c42be6b755abfffed606e5d4a46f8ac511a203a
GalinaKr <kravchukgi@gmail.com>
2021-05-30 12:55:31 +0300

    new one

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -3,20 +3,18 @@ require File.expand_path('../production.rb', __FILE__)
 
 Rails.application.configure do
   # Here override any defaults
+  config.app_domain = 'staging-resume-znpp.herokuapp.com'
 
-  host = ENV['DOMAIN_URL']
-
-  config.action_mailer.default_url_options = { :host => 'https://staging-resume-znpp.herokuapp.com' }
+  # Email
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default_url_options = { host: config.app_domain }
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => ENV["GMAIL_USERNAME"],
-    :password             => ENV["GMAIL_PASSWORD"],
-    :authentication       => "plain",
-    :enable_starttls_auto => true
-  }
+    address: 'smtp.gmail.com',
+    port: '587',
+    enable_starttls_auto: true,
+    user_name: 'resume.znpp@gmail.com',
+    password: '!1Qwerty',
+    authentication: :plain,
+    domain: 'staging-resume-znpp.herokuapp.com'}
 end
