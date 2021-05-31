32706cf54e58ba4ebeae1bf69923845d3a640b55
GalinaKr <kravchukgi@gmail.com>
2021-05-31 17:23:47 +0300

    try to send reset letter

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -2,6 +2,9 @@
 require File.expand_path('../production.rb', __FILE__)
 
 Rails.application.configure do
+
+  host = ENV['DOMAIN_URL']
+  
   # Here override any defaults
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
@@ -11,4 +14,11 @@ Rails.application.configure do
     user_name:            'resume.znpp@gmail.com',
     password:             '!1Qwerty',
     authentication:       'plain' }
+
+  config.action_mailer.preview_path = "#{Rails.root}/test/mailers/previews"
+  config.action_mailer.asset_host = "http://#{host}"
+  config.action_mailer.default_url_options = { host: host }
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = false
 end
